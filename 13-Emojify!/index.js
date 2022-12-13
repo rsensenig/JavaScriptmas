cb7595245f70f137bb9f537faf517cbea85bc770
rsensenig <r.a.sensenig@gmail.com>
2022-12-12 22:10:29 -0500

    began to create emojifyWord function

---
+++ 13-Emojify!/index.js
@@ -0,0 +1,90 @@
+/*  Emojify!
+
+Popular services like Slack and Github allow for emoji shortscodes, meaning 
+they will detect when a word in a sentence begins and ends with a colon (:)
+and automatically replace that word with an emoji. 
+
+These shortcodes allow users to add an emoji to their messages by typing a 
+code rather than searching for an emoji from a list. 
+
+For example, typing :smile: will replace that text with 😊 
+
+*/
+
+const emojis = {
+    "smile": "😊",
+    "angry": "😠",
+    "party": "🎉",
+    "heart": "💜",
+    "cat":   "🐱",
+    "dog":   "🐕"
+}
+
+/* 1. Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, remove the colons and
+look up the word in the emoji object. If the word is in the 
+emojis object, return the corresponding emoji.
+If it isn't, return the original word.
+
+Example input: ":party:"
+Example output: 🎉
+
+Example input: ":flower:"
+Example output: "flower"
+
+Example input: "elephant"
+Example output: "elephant"
+*/ 
+
+function emojifyWord(word){
+    // convert word to lowercase
+    word = word.toLowerCase();
+
+    // if the word starts and ends with a colon
+    if(word.startsWith(":") && word.endsWith(":")) {
+        // then remove the colons
+        word = word.slice(1, -1);
+
+        // look up the word in the emoji object
+        emojis.forEach(emoji => {
+            // if the word is in the emoji object
+            if(emoji === "smile") {
+                // return the corresponding emoji
+                return "😊";
+            } else {
+                // else return the original word
+                return word;
+            }
+        });
+
+    }
+
+    return word;
+}
+
+console.log(emojifyWord(":smile:"));
+
+/* 2. Write a function to find any emoji shortcodes in a phrase.
+Your function should map over each word in the phrase, emojify any word
+that begins and ends with a colon, then return the emojified phrase. 
+Feel free to use your emojify function from the previous exercise!
+
+Example input: "I :heart: my :cat:"
+Example output: "I 💜 my 🐱"
+
+Example input: "I :heart: my elephant"
+Example output: "I 💜 my elephant"
+*/ 
+
+function emojifyPhrase(phrase){
+    return;
+}
+
+
+
+// console.log(emojifyWord(":heart:"));
+// console.log(emojifyWord(":flower:"));
+// console.log(emojifyWord("elephant"));
+
+// console.log(emojifyPhrase("I :heart: my :cat:"));
+// console.log(emojifyPhrase("I :heart: my :elephant:"));
